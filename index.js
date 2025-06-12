bc969cca601c0588af88c87adab8f1e97fe375a7
robloxredirect <start11freetrial@gmail.com>
2025-06-12 12:22:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var newlocation = window.location.href
-if (newlocation === "https://digidalstudios.github.io/") {
+if (newlocation === "https://robloxredirect.github.io/") {
   window.location.href = "roblox://"
   throw ''
 }
-window.location.href = newlocation.replace("https://digidalstudios.github.io/?", "roblox://")
+window.location.href = newlocation.replace("https://robloxredirect.github.io/?", "roblox://")
 setTimeout(() => {
   window.close()
 }, "500");
